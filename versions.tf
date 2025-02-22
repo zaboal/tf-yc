08d5a23a7dd09898179d8b84298a1acd2aa5fd55
Bogdan Zažigin <zaboal@tuta.io>
2025-02-22 19:47:41 +0300

    Update versions (#2)
    * Update providers' versions
    
    * Update Terraform's version as well

--- versions.tf
+++ versions.tf
@@ -1,19 +1,18 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.10"
 
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = ">= 0.107.0"
+      version = ">= 0.138"
     }
-
     random = {
       source  = "hashicorp/random"
-      version = "> 3.3"
+      version = ">= 3.6"
     }
     time = {
       source  = "hashicorp/time"
-      version = "> 0.9"
+      version = ">= 0.12"
     }
   }
 }
